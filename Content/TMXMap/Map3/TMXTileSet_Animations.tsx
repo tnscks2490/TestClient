e5ba56342c48a467e57c033fcacec7357931f3be
tnscks2490 <112551499+tnscks2490@users.noreply.github.com>
2024-11-25 21:14:22 +0900

    추가

---
+++ Content/TMXMap/Map3/TMXTileSet_Animations.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TMXTileSet_Animations" tilewidth="16" tileheight="16" tilecount="7308" columns="126">
+ <image source="TMXTileSet_Animations.png" width="2016" height="928"/>
+</tileset>

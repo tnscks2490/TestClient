e07b6b8518bfd93aaf4c44fe3f65a0fa3ae94a12
Calvin Lee <lsc2490@naver.com>
2024-11-26 14:28:29 +0900

    추가

---
+++ Content/TMXMap/Python/TMXTileSet_Animations.tsx
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="TMXTileSet_Animations" tilewidth="16" tileheight="16" tilecount="7308" columns="126">
+ <image source="TMXTileSet_Animations.png" width="2016" height="928"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="724" duration="200"/>
+   <frame tileid="729" duration="200"/>
+   <frame tileid="734" duration="200"/>
+   <frame tileid="739" duration="200"/>
+   <frame tileid="744" duration="200"/>
+   <frame tileid="749" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1253">
+  <animation>
+   <frame tileid="724" duration="200"/>
+   <frame tileid="729" duration="200"/>
+   <frame tileid="734" duration="200"/>
+   <frame tileid="739" duration="200"/>
+   <frame tileid="744" duration="200"/>
+   <frame tileid="749" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1254">
+  <animation>
+   <frame tileid="725" duration="200"/>
+   <frame tileid="730" duration="200"/>
+   <frame tileid="735" duration="200"/>
+   <frame tileid="740" duration="200"/>
+   <frame tileid="745" duration="200"/>
+   <frame tileid="750" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1255">
+  <animation>
+   <frame tileid="726" duration="200"/>
+   <frame tileid="731" duration="200"/>
+   <frame tileid="736" duration="200"/>
+   <frame tileid="741" duration="200"/>
+   <frame tileid="746" duration="200"/>
+   <frame tileid="751" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1256">
+  <animation>
+   <frame tileid="727" duration="200"/>
+   <frame tileid="732" duration="200"/>
+   <frame tileid="737" duration="200"/>
+   <frame tileid="742" duration="200"/>
+   <frame tileid="747" duration="200"/>
+   <frame tileid="752" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1257">
+  <animation>
+   <frame tileid="728" duration="200"/>
+   <frame tileid="733" duration="200"/>
+   <frame tileid="738" duration="200"/>
+   <frame tileid="743" duration="200"/>
+   <frame tileid="748" duration="200"/>
+   <frame tileid="753" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1379">
+  <animation>
+   <frame tileid="850" duration="200"/>
+   <frame tileid="855" duration="200"/>
+   <frame tileid="860" duration="200"/>
+   <frame tileid="865" duration="200"/>
+   <frame tileid="870" duration="200"/>
+   <frame tileid="875" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1381">
+  <animation>
+   <frame tileid="852" duration="200"/>
+   <frame tileid="857" duration="200"/>
+   <frame tileid="862" duration="200"/>
+   <frame tileid="867" duration="200"/>
+   <frame tileid="872" duration="200"/>
+   <frame tileid="877" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1382">
+  <animation>
+   <frame tileid="853" duration="200"/>
+   <frame tileid="858" duration="200"/>
+   <frame tileid="863" duration="200"/>
+   <frame tileid="868" duration="200"/>
+   <frame tileid="873" duration="200"/>
+   <frame tileid="878" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1383">
+  <animation>
+   <frame tileid="854" duration="200"/>
+   <frame tileid="859" duration="200"/>
+   <frame tileid="864" duration="200"/>
+   <frame tileid="869" duration="200"/>
+   <frame tileid="874" duration="200"/>
+   <frame tileid="879" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1505">
+  <animation>
+   <frame tileid="976" duration="200"/>
+   <frame tileid="981" duration="200"/>
+   <frame tileid="986" duration="200"/>
+   <frame tileid="991" duration="200"/>
+   <frame tileid="996" duration="200"/>
+   <frame tileid="1001" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1506">
+  <animation>
+   <frame tileid="977" duration="200"/>
+   <frame tileid="982" duration="200"/>
+   <frame tileid="987" duration="200"/>
+   <frame tileid="992" duration="200"/>
+   <frame tileid="997" duration="200"/>
+   <frame tileid="1002" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1507">
+  <animation>
+   <frame tileid="978" duration="200"/>
+   <frame tileid="983" duration="200"/>
+   <frame tileid="988" duration="200"/>
+   <frame tileid="993" duration="200"/>
+   <frame tileid="998" duration="200"/>
+   <frame tileid="1003" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
